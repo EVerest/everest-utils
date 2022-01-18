be00ba9d3de6f1234fcf7889aefadbe944791edd
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2022-01-18 19:49:12 +0100

    Improve init.sh error handling and add info output for users
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- everest-cpp/init.sh
+++ everest-cpp/init.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 ##
 ## SPDX-License-Identifier: Apache-2.0
-## Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
+## Copyright 2020 - 2022 Pionix GmbH and Contributors to EVerest
 ##
 echo "Installing required tools, initializing workspace, changing into everest-core/build"
-git clone git@github.com:EVerest/everest-dev-environment.git
-echo "PATH=/home/docker/.local/bin:\$PATH" >> /home/docker/.profile
-. /home/docker/.profile
+if [ -d "everest-dev-environment" ] ; then
+    echo "Directory 'everest-dev-environment' already exists, trying to update its contents..."
+    (cd everest-dev-environment && git pull)
+else
+    git clone git@github.com:EVerest/everest-dev-environment.git || { echo "Could not clone everest-dev-environment. Did you add your SSH key on GitHub and made it available to ssh-agent?"; return; }
+fi
+PROFILE_PATH="$HOME/.profile"
+CPM_SOURCE_CACHE_VAR="export CPM_SOURCE_CACHE=\$HOME/cache"
+grep -qF -- "$CPM_SOURCE_CACHE_VAR" "$PROFILE_PATH" || echo "$CPM_SOURCE_CACHE_VAR" >> "$PROFILE_PATH"
 (cd everest-dev-environment/dependency_manager && python3 -m pip install .)
+echo "Sourcing '$PROFILE_PATH'"
+. "$PROFILE_PATH"
 edm --register-cmake-module
 edm --config everest-dev-environment/everest-complete.yaml --workspace .
 edm --update
 (cd everest-utils/ev-dev-tools && python3 -m pip install .)
 mkdir -p everest-core/build
 cd everest-core/build
+echo ""
+echo "You can now build everest-core with the following commands:"
+echo ""
+echo "cmake .. && make install -j`nproc`"
