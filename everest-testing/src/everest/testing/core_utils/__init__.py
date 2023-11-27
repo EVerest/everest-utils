7df2949802545bbdb783891ebaf83ac117df2668
Fabian Klemm <fabian.klemm@pionix.de>
2023-11-27 10:13:15 +0100

    refactor: rename to strategy pattern
    Signed-off-by: Fabian Klemm <fabian.klemm@pionix.de>

---
+++ everest-testing/src/everest/testing/core_utils/__init__.py
@@ -0,0 +1,4 @@
+from ._configuration.everest_configuration_strategies.everest_configuration_strategy import EverestConfigAdjustmentStrategy
+
+__all__ = ["common", "everest_core", "fixtures", "probe_module"]
+
