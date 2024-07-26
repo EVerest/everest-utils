4a0c3d828a622c68a443ce73664d9786f12ec72c
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-07-26 14:12:24 +0200

    Remove example configs except the config-fallback.yaml
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- docker/everest-docker-image/run-config-fallback.sh
+++ docker/everest-docker-image/run-config-fallback.sh
@@ -4,13 +4,13 @@
 
 usage() {
     echo "Usage: $0 [--conf <string>] [--ocpp-conf <string>] [--init]" 1>&2
-    echo -e "\t--conf: Path to EVerest config file - Optional, defaults to config-ac-pwm.yaml"
+    echo -e "\t--conf: Path to EVerest config file - Optional, defaults to config-fallbackyaml"
     echo -e "\t--ocpp-conf: Path to EVerest OCPP config file - Optional, defaults to ocpp-config.json"
     echo -e "\t--init: If the OCPP 2.0.1 device model should be re-initialized with the given OCPP config - Optional"
     exit 1
 }
 
-export EVEREST_CONFIG=config-ac-pwm.yaml
+export EVEREST_CONFIG=config-fallbackyaml.yaml
 export OCPP_CONFIG=ocpp-config.json
 export EVEREST_COMMAND="sh -c '/opt/everest/bin/manager --conf /opt/everest/config/config.yaml'"
 
