b5ed50c39eafbaec0908770c2acc3ef7e44ae32b
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2025-01-27 14:17:36 +0100

    use '' instead of ""
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- ev-dev-tools/src/ev_coverage/cov.py
+++ ev-dev-tools/src/ev_coverage/cov.py
@@ -35,21 +35,21 @@ def remove_wrapper(filepath: pathlib.Path, dry_run=True, silent=False):
 
 
 def remove_all_gcda_files(build_dir: str, dry_run: bool, silent: bool) -> int:
-    log_wrapper("Removing all gcda files from build directory", silent)
+    log_wrapper('Removing all gcda files from build directory', silent)
 
     dir = pathlib.Path(build_dir)
-    return len([remove_wrapper(f, dry_run, silent) for f in dir.rglob("*.gcda")])
+    return len([remove_wrapper(f, dry_run, silent) for f in dir.rglob('*.gcda')])
 
 
 def remove_orphaned_object_files(build_dir: str, source_dirs: list[str], dry_run: bool, silent: bool) -> int:
-    log_wrapper("Removing orphaned object files and gcno files from build directory", silent)
+    log_wrapper('Removing orphaned object files and gcno files from build directory', silent)
     # Find all object files (*.o) in the build directory
-    object_files = glob.glob(f"{build_dir}/**/*.o", recursive=True)
+    object_files = glob.glob(f'{build_dir}/**/*.o', recursive=True)
     removed_files = 0
 
     for obj_file in object_files:
         # 1. Remove everything in the path until "*.dir/"
-        strip_substr = ".dir" + os.sep
+        strip_substr = '.dir' + os.sep
         split_len = obj_file.find(strip_substr) + len(strip_substr)
         relative_path_obj = obj_file[split_len:]
 
@@ -64,7 +64,7 @@ def remove_orphaned_object_files(build_dir: str, source_dirs: list[str], dry_run
         obj_basename = os.path.basename(obj_file)
 
         # Convert the object file base name to the corresponding cpp file name
-        cpp_basename = obj_basename[:-2] if obj_basename.endswith("cpp.o") else obj_basename.replace(".o", ".cpp")
+        cpp_basename = obj_basename[:-2] if obj_basename.endswith('cpp.o') else obj_basename.replace('.o', '.cpp')
 
         relative_dir = os.path.split(relative_path_obj)
         relative_path_cpp = relative_dir[0] + os.sep + cpp_basename
@@ -85,11 +85,11 @@ def remove_orphaned_object_files(build_dir: str, source_dirs: list[str], dry_run
 
         # If no corresponding .cpp file was found, remove the orphaned .o file
         if not cpp_file_exists:
-            log_wrapper(f"Removing orphaned object file: {obj_file}", silent)
+            log_wrapper(f'Removing orphaned object file: {obj_file}', silent)
             try:
-                for p in pathlib.Path(build_dir).rglob(relative_path_cpp + "*"):
+                for p in pathlib.Path(build_dir).rglob(relative_path_cpp + '*'):
                     if os.path.isfile(p):
-                        log_wrapper(f"Removing other orphaned file: {p}", silent)
+                        log_wrapper(f'Removing other orphaned file: {p}', silent)
                         remove_wrapper(p, dry_run, silent)
                         removed_files += 1
             except NotImplementedError:
@@ -98,7 +98,7 @@ def remove_orphaned_object_files(build_dir: str, source_dirs: list[str], dry_run
                 pass
             # If obj file is not removed in the previous loop, remove it now
             if os.path.isfile(obj_file):
-                log_wrapper(f"Removing object file: {obj_file}", silent)
+                log_wrapper(f'Removing object file: {obj_file}', silent)
                 remove_wrapper(obj_file, dry_run, silent)
                 removed_files += 1
     return removed_files
@@ -122,12 +122,12 @@ def main():
 
 
     parser_file_remover.add_argument('--source-dirs', nargs='+', type=str, action='extend', required=True,
-                        help="Source files directories to search in for cpp files. Can be multiple, "
-                             "separated by a space.")
-    parser_file_remover.add_argument('--build-dir', type=str, required=True, help="Build directory")
-    parser_file_remover.add_argument('--dry-run', required=False, action='store_true', help="Dry run, does not remove any files", default=False)
-    parser_file_remover.add_argument('--summary', required=False, action='store_true', help="Only show a summary of removed files", default=False)
-    parser_file_remover.add_argument('--silent', required=False, action='store_true', help="Suppress all output, summary is still shown when requested", default=False)
+                        help='Source files directories to search in for cpp files. Can be multiple, '
+                             'separated by a space.')
+    parser_file_remover.add_argument('--build-dir', type=str, required=True, help='Build directory')
+    parser_file_remover.add_argument('--dry-run', required=False, action='store_true', help='Dry run, does not remove any files', default=False)
+    parser_file_remover.add_argument('--summary', required=False, action='store_true', help='Only show a summary of removed files', default=False)
+    parser_file_remover.add_argument('--silent', required=False, action='store_true', help='Suppress all output, summary is still shown when requested', default=False)
     parser_file_remover.set_defaults(action_handler=remove_unnecessary_files)
     args = parser.parse_args()
 
