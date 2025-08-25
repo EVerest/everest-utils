a96df762ceebe8060b52a1a823459f8545a3018c
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2025-08-25 18:45:02 +0200

    Add replace_license script
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

---
+++ scripts/replace_license.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Pionix GmbH and Contributors to EVerest
+#
+"""
+author: kai-uwe.hermann@pionix.de
+Replace licenses with Apache 2.0
+"""
+
+import argparse
+from datetime import date
+from pathlib import Path
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='create an isolated snapshot with edm')
+
+    parser.add_argument('--working-dir', '-wd', type=str,
+                        help='Working directory (default: .)', default=str(Path.cwd()))
+    parser.add_argument('--no-year', action='store_true', help='Do not include years in license header')
+
+    args = parser.parse_args()
+
+    working_dir = Path(args.working_dir).expanduser().resolve()
+
+    files = [file for file in working_dir.rglob('*') if file.suffix in ['.cpp', '.hpp']]
+
+    year = ''
+    if not args.no_year:
+        year = f'2020 - {date.today().year} '
+
+    license_text = f"""// SPDX-License-Identifier: Apache-2.0
+// Copyright {year}Pionix GmbH and Contributors to EVerest
+"""
+
+    success = 0
+    failure = 0
+    count = len(files)
+
+    for file in files:
+        content = file.read_text()
+        if content.startswith('/*'):
+            needle = '*/\n'
+            end = content.find(needle) + len(needle)
+            new_content = license_text + content[end:]
+            file.write_text(new_content)
+            print(f'Modified {file} with new license header')
+            success += 1
+        else:
+            content_lines = content.splitlines()
+            end = 0
+            for line in content_lines:
+                if line.startswith('//'):
+                    end += 1
+                else:
+                    break
+            new_content = license_text + '\n'.join(content_lines[end:]) + '\n'
+            file.write_text(new_content)
+            success += 1
+    
+    if success != count:
+        print('ERROR during license replacement')
+    else:
+        print('Everything went well')
+
+if __name__ == '__main__':
+    main()
